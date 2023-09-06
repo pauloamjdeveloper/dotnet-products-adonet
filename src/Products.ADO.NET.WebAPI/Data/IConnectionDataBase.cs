6878c77a001f70d953398e15bdef738095f42f5d
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-06 09:58:29 -0300

    Criando conexão com o banco de dados

---
+++ src/Products.ADO.NET.WebAPI/Data/IConnectionDataBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Products.ADO.NET.WebAPI.Data
+{
+    public interface IConnectionDataBase
+    {
+        IDbConnection GetDbConnection();
+
+        void OpenConnection();
+
+        void CloseConnection();
+    }
+}
---
+++ src/Products.ADO.NET.WebAPI/Data/Implementations/SQLServerDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Products.ADO.NET.WebAPI.Data.Implementations
+{
+    public class SQLServerDatabase : IConnectionDataBase
+    {
+        private readonly string _connectionString;
+
+        public SQLServerDatabase()
+        {
+            _connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=DB_ProductsADONET;User ID=sa;Password=********";
+        }
+        
+        public IDbConnection GetDbConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            using (var connection = GetDbConnection() as SqlConnection) 
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException exception)
+                {
+                    throw new Exception("Error opening connect to database: " + exception.Message);
+                }
+            }
+        }
+        public void CloseConnection()
+        {
+            using (var connection = GetDbConnection() as SqlConnection)
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch (SqlException exception)
+                {
+                    throw new Exception("Error closing database connection: " + exception.Message);
+                }
+            }
+        }
+    }
+}
