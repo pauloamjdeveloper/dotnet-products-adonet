76b1ca4a63037cfc023c054c6afcd4d957a5b422
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-07 15:15:44 -0300

    Aplicando a regra de negócio do produto

---
+++ src/Products.ADO.NET.WebAPI/Business/IProductBusiness.cs
@@ -0,0 +1,19 @@
+﻿using Products.ADO.NET.WebAPI.Models;
+
+namespace Products.ADO.NET.WebAPI.Business
+{
+    public interface IProductBusiness
+    {
+        List<Product> GetAllProducts();
+
+        Product GetProductById(int id);
+
+        Product CreateProduct(Product product);
+
+        Product UpdateProduct(Product product);
+
+        void DeleteProduct(int id);
+
+        bool Exists(int id);
+    }
+}
---
+++ src/Products.ADO.NET.WebAPI/Business/Implementations/ProductBusiness.cs
@@ -0,0 +1,46 @@
+﻿using Products.ADO.NET.WebAPI.Models;
+using Products.ADO.NET.WebAPI.Repositories;
+
+namespace Products.ADO.NET.WebAPI.Business.Implementations
+{
+    public class ProductBusiness : IProductBusiness
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductBusiness(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _productRepository.GetAll();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _productRepository.GetById(id);
+        }
+
+        public Product CreateProduct(Product product)
+        {
+            return _productRepository.Create(product);
+        }
+
+        public Product UpdateProduct(Product product)
+        {
+            return _productRepository.Update(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            _productRepository.Delete(id);
+        }
+
+        public bool Exists(int id)
+        {
+            var product = _productRepository.GetById(id);
+            return product != null;
+        }
+    }
+}
