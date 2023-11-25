38e38551379967a5d07215acb249a25e57b8872f
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-11-24 21:51:56 -0300

    Atualizando os métodos do repositório de produtos

--- src/Products.ADO.NET.WebAPI/Repositories/Implementations/ProductRepository.cs
+++ src/Products.ADO.NET.WebAPI/Repositories/Implementations/ProductRepository.cs
@@ -10,6 +10,10 @@ namespace Products.ADO.NET.WebAPI.Repositories.Implementations
     {
         private readonly IConnectionDataBase _dataBase;
         private readonly ILogger<ProductRepository> _logger;
+        private string _errorMessage = "";
+
+        IDbConnection _connection = null;
+        SqlDataReader dataReader = null;
 
         public ProductRepository(IConnectionDataBase dataBase, ILogger<ProductRepository> logger)
         {
@@ -17,42 +21,37 @@ namespace Products.ADO.NET.WebAPI.Repositories.Implementations
             _logger = logger;
         }
 
-        private string _errorMessage = "";
-
         public List<Product> GetAll()
         {
-            IDbConnection connection = null;
-
             try
             {
                 var products = new List<Product>();
+                var command = new SqlCommand();
 
-                connection = _dataBase.GetDbConnection();
-                connection.Open();
+                _connection = _dataBase.GetDbConnection();
 
-                string query = "SELECT * FROM TB_Products";
+                command.CommandText = "SELECT * FROM TB_Products";
+                command.Connection = (SqlConnection)_connection;
 
-                using (var command = new SqlCommand(query, (SqlConnection)connection))
+                _connection.Open();
+
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
                 {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            var id = Convert.ToInt32(reader["Id"]);
-                            var name = Convert.ToString(reader["Name"]);
-                            var description = Convert.ToString(reader["Description"]);
-                            var price = Convert.ToDecimal(reader["Price"]);
-                            var quantity = Convert.ToInt32(reader["Quantity"]);
+                    var id = Convert.ToInt32(dataReader["Id"]);
+                    var name = Convert.ToString(dataReader["Name"]);
+                    var description = Convert.ToString(dataReader["Description"]);
+                    var price = Convert.ToDecimal(dataReader["Price"]);
+                    var quantity = Convert.ToInt32(dataReader["Quantity"]);
 
-                            var product = new Product(id, name, description, price, quantity);
-                            products.Add(product);
-                        }
-                    }
+                    var product = new Product(id, name, description, price, quantity);
+                    products.Add(product);
                 }
 
                 return products;
             }
-            catch (SqlException exception)
+            catch (Exception exception)
             {
                 _errorMessage = $"Error getting all products: {exception.Message}";
                 _logger.LogError(exception, _errorMessage);
@@ -60,160 +59,200 @@ namespace Products.ADO.NET.WebAPI.Repositories.Implementations
             }
             finally
             {
-                connection.Close();
+                _connection.Close();
             }
         }
 
 
         public Product GetById(int id)
         {
-            IDbConnection connection = null;
-
             try
             {
-                connection = _dataBase.GetDbConnection();
-                connection.Open();
+                var command = new SqlCommand();
 
-                var query = "SELECT * FROM TB_Products WHERE Id = @Id";
+                _connection = _dataBase.GetDbConnection();
+                _connection.Open();
 
-                using (var command = new SqlCommand(query, (SqlConnection)connection)) 
+                command.CommandText = "SELECT * FROM TB_Products WHERE Id = @Id";
+                command.Connection = (SqlConnection)_connection;
+                command.Parameters.AddWithValue(@"Id", id);
+
+                dataReader = command.ExecuteReader();
+
+                if (dataReader.Read())
                 {
-                    command.Parameters.AddWithValue(@"Id", id);
+                    var name = Convert.ToString(dataReader["Name"]);
+                    var description = Convert.ToString(dataReader["Description"]);
+                    var price = Convert.ToDecimal(dataReader["Price"]);
+                    var quantity = Convert.ToInt32(dataReader["Quantity"]);
 
-                    using (var reader = command.ExecuteReader()) 
-                    {
-                        if (reader.Read())
-                        {
-                            var name = Convert.ToString(reader["Name"]);
-                            var description = Convert.ToString(reader["Description"]);
-                            var price = Convert.ToDecimal(reader["Price"]);
-                            var quantity = Convert.ToInt32(reader["Quantity"]);
-
-                            var product = new Product(id, name, description, price, quantity);
-                            return product;
-                        }
-                        else 
-                        {
-                            throw new ProductNotFoundException($"Product with ID {id} not found.");
-                        }
-                    }
+                    var product = new Product(id, name, description, price, quantity);
+                    return product;
+                }
+                else
+                {
+                    throw new ProductNotFoundException($"Product with ID {id} not found.");
                 }
             }
-            catch (SqlException exception)
+            catch (Exception exception)
             {
                 _errorMessage = $"Error getting product by id: {exception.Message}";
                 _logger.LogError(exception, _errorMessage);
                 throw new Exception(_errorMessage);
             }
-            finally 
+            finally
             {
-                connection.Close();
+                _connection.Close();
             }
         }
 
         public Product Create(Product product)
         {
-            IDbConnection connection = null;
+            _connection = _dataBase.GetDbConnection();
+            _connection.Open();
+
+            var transaction = (SqlTransaction)_connection.BeginTransaction();
 
             try
             {
-                connection = _dataBase.GetDbConnection();
-                connection.Open();
+                var command = new SqlCommand();
 
-                using (var command = (SqlCommand)connection.CreateCommand())
-                {
-                    command.CommandText = "INSERT INTO TB_Products (Name, Description, Price, Quantity) VALUES (@Name, @Description, @Price, @Quantity)";
+                command.Transaction = transaction;
+                command.Connection = (SqlConnection)_connection;
+                command.CommandText = "INSERT INTO TB_Products (Name, Description, Price, Quantity) VALUES (@Name, @Description, @Price, @Quantity)";
 
-                    AddParameter(command, "@Name", SqlDbType.NVarChar, product.Name);
-                    AddParameter(command, "@Description", SqlDbType.NVarChar, product.Description);
-                    AddParameter(command, "@Price", SqlDbType.Decimal, product.Price);
-                    AddParameter(command, "@Quantity", SqlDbType.Int, product.Quantity);
+                AddParameter(command, "@Name", DbType.String, product.Name);
+                AddParameter(command, "@Description", DbType.String, product.Description);
+                AddParameter(command, "@Price", DbType.Decimal, product.Price);
+                AddParameter(command, "@Quantity", DbType.Int32, product.Quantity);
 
-                    command.ExecuteNonQuery();
+                command.ExecuteNonQuery();
 
-                    return product;
-                }
+                transaction.Commit();
+
+                return product;
             }
-            catch (SqlException exception)
+            catch (Exception exception)
             {
+                try
+                {
+                    transaction.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
                 _errorMessage = $"Error creating product: {exception.Message}";
                 _logger.LogError(exception, _errorMessage);
                 throw new Exception(_errorMessage);
+
             }
-            finally 
+            finally
             {
-                connection.Close();
+                _connection.Close();
             }
         }
 
         public Product Update(Product product)
         {
-            IDbConnection connection = null;
+            _connection = _dataBase.GetDbConnection();
+            _connection.Open();
+
+            var transaction = (SqlTransaction)_connection.BeginTransaction();
 
             try
             {
-                connection = _dataBase.GetDbConnection();
-                connection.Open();
+                var command = new SqlCommand();
 
-                using (var command = (SqlCommand)connection.CreateCommand()) 
-                {
-                    command.CommandText = "UPDATE TB_Products SET Name = @Name, Description = @Description, Price = @Price, Quantity = @Quantity WHERE Id = @Id";
+                command.Transaction = transaction;
+                command.Connection = (SqlConnection)_connection;
+                command.CommandText = "UPDATE TB_Products SET Name = @Name, Description = @Description, Price = @Price, Quantity = @Quantity WHERE Id = @Id";
 
-                    AddParameter(command, "@Id", SqlDbType.Int, product.Id);
-                    AddParameter(command, "@Name", SqlDbType.NVarChar, product.Name);
-                    AddParameter(command, "@Description", SqlDbType.NVarChar, product.Description);
-                    AddParameter(command, "@Price", SqlDbType.Decimal, product.Price);
-                    AddParameter(command, "@Quantity", SqlDbType.Int, product.Quantity);
+                AddParameter(command, "@Id", DbType.Int32, product.Id);
+                AddParameter(command, "@Name", DbType.String, product.Name);
+                AddParameter(command, "@Description", DbType.String, product.Description);
+                AddParameter(command, "@Price", DbType.Decimal, product.Price);
+                AddParameter(command, "@Quantity", DbType.Int32, product.Quantity);
 
-                    command.ExecuteNonQuery();
+                command.ExecuteNonQuery();
 
-                    return product;
-                }
+                transaction.Commit();
+
+                return product;
             }
-            catch (SqlException exception)
+            catch (Exception exception)
             {
+                try
+                {
+                    transaction.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
                 _errorMessage = $"Error updating product: {exception.Message}";
                 _logger.LogError(exception, _errorMessage);
                 throw new Exception(_errorMessage);
+
             }
             finally
             {
-                connection.Close();
+                _connection.Close();
             }
+
         }
 
         public void Delete(int id)
         {
-            IDbConnection connection = null;
+
+            _connection = _dataBase.GetDbConnection();
+            _connection.Open();
+
+            var transaction = (SqlTransaction)_connection.BeginTransaction();
 
             try
             {
-                connection = _dataBase.GetDbConnection();
-                connection.Open();
+                var command = new SqlCommand();
 
-                using (var command = (SqlCommand)connection.CreateCommand()) 
+                command.Transaction = transaction;
+                command.Connection = (SqlConnection)_connection;
+                command.CommandText = "DELETE FROM TB_Products WHERE Id = @Id";
+
+                AddParameter(command, "@id", DbType.Int32, id);
+
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+            }
+            catch (Exception exception)
+            {
+                try
                 {
-                    command.CommandText = "DELETE FROM TB_Products WHERE Id = @Id";
-                    AddParameter(command, "@Id", SqlDbType.Int, id);
-                    command.ExecuteNonQuery();
+                    transaction.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
                 }
 
-            }
-            catch (SqlException exception)
-            {
                 _errorMessage = $"Error deleting product: {exception.Message}";
                 _logger.LogError(exception, _errorMessage);
                 throw new Exception(_errorMessage);
+
             }
             finally
             {
-                connection.Close();
+                _connection.Close();
             }
         }
 
-        private void AddParameter(SqlCommand command, string paramName, SqlDbType dbType, object value)
+        private void AddParameter(IDbCommand command, string paramName, DbType dbType, object value)
         {
-            var parameter = new SqlParameter(paramName, dbType) { Value = value };
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = paramName;
+            parameter.DbType = dbType;
+            parameter.Value = value;
             command.Parameters.Add(parameter);
         }
     }
